543603125c88a6ba75b979dd65a9110965707331
Christian Figueiredo <developer.figueiredo@gmail.com>
2022-01-21 23:36:38 +0000

    criacao do path no index

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require("path");
+
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/home', (req, res) => {
-  res.send('Hello World')
-})
+  res.render('index');
+});
 
-app.listen(3000)
+app.listen(port, () => console.log(` Servidor rodando em http://localhost:${port}/home`)
+);
